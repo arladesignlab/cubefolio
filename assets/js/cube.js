0adb849b5be191184e7f802993dd8fbccadaa37c
Diego Ponce <diegoponceg@hotmail.com>
2015-03-02 20:17:38 -0600

    Agregada funcionalidad al boton FeaturedNode-scrollButton, linkeado el menu

--- assets/js/cube.js
+++ assets/js/cube.js
@@ -1,53 +1,24 @@
 $(function() {
   //Variables
   oneSubmenu=true;
-  $('.NoMenu').click(function(){
-    $('.MainMenu-subMenu').slideUp("fast");
-  });
-  
   $('.MainMenu-mobileMenu').click(function(event){
     event.preventDefault();
     $('.MainMenu-list').slideToggle("fast");
-  });
-  $('.MainMenu-item').on("click",function(event){
-    event.preventDefault();
-    if ($(this).hasClass("active")==true) {
-      $('.MainMenu-subMenu').slideUp("fast");
-      $(this).removeClass("active");
-      oneSubmenu=true;
-
-    }
-    else
-    {
-      if (oneSubmenu==false){
-        $('.MainMenu-subMenu').slideUp("fast");
-        $('.MainMenu-item').removeClass("active");
-        $(this).children('.MainMenu-subMenu').slideDown("fast");
-        $(this).addClass("active");
-
-      }
-      else
-      {
-        $(this).addClass("active");
-        $(this).children('.MainMenu-subMenu').slideDown("fast");
-        oneSubmenu=false;
-      }
-      
-    }
   });
   $('#slides').click(function(){
     $('.MainMenu-subMenu').slideUp("fast");
     var sizeWindow = $(window).width();
     console.log(sizeWindow);
     if (sizeWindow<768){
-      $('.MainMenu-list').slideUp("fast");  
+      $('.MainMenu-list').slideUp("fast");
     };
   });
-  function featuredImages(){ 
+  function featuredImages(){
     var containers = $('.FeaturedNode-imageContainer');
+    var altoContainer = $('.FeaturedNode-imageContainer').width()*0.612;
+    $('.FeaturedNode-imageContainer').css('height',altoContainer);
       for (var i = 0; i < containers.length; i++) {
-        var altoContainer = $(containers[i]).height();
-        var altoImg = $(containers[i]).children('img').height();
+        var altoImg = $(containers[i]).children('.FeaturedNode-image').height();
         if (altoImg<altoContainer) {
             console.log("la imagen "+i+" es mas chica que el container")
             var scala= altoContainer/altoImg;
@@ -55,9 +26,15 @@ $(function() {
             console.log("se tiene que escalar "+scala+" y el margen de "+margen);
             $(containers[i]).children('.FeaturedNode-image').css("transform","scale("+scala+")");
             $(containers[i]).children('.FeaturedNode-image').css("margin",margen);
-            
-        };        
+
+        };
       };
     };
+    $(window).resize(function(){
+      var altoContainer = $('.FeaturedNode-imageContainer').width()*0.612;
+      $('.FeaturedNode-imageContainer').css('height',altoContainer);
+      $('.FeaturedNode-imageContainer').children('.FeaturedNode-image').css("height",altoContainer);
+
+    });
   featuredImages();
 });
